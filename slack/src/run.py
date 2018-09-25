1f753dc8c48e273e413cdf846be0315a3734bec1
qh53 <qh53@cornell.edu>
2018-09-25 13:21:00 -0400

    fix

--- slack/src/run.py
+++ slack/src/run.py
@@ -2,9 +2,9 @@ from app import app
 
 
 if __name__ == '__main__':
-  PORT = 5000
+  PORT = 8080
   HOST = '0.0.0.0'
   print 'Server running on {}:{}'.format(HOST, PORT)
-  app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=8080)
 
 
