4f74d1af210a5651d48beb749a576a6790260d98
Jane Zhang <35616046+jz393@users.noreply.github.com>
2018-10-12 20:45:14 -0400

    use faster textrank implementation

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import slack.slack_bot
 import slack.slack_bot.app as app 
 from textrank import get_summary
+from textrank2 import get_summary2
 
 def process_text(text):
-    info = get_summary(text)
-    return "Keywords: " + ",".join(info[0]) + "\nSummary: too slow to show..." 
+    keywords = get_summary(text)[0]
+    summary = get_summary2(text) #change number of sentences?
+    return "Keywords: {} \nSummary: {}".format(", ".join(keywords), summary) 
 
 app.process_text = process_text
 
